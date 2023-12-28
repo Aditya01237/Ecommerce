fdbbe9436b225dabab8862293b63c6790ece053a
ADITYA PAREEK <80575917+Aditya01237@users.noreply.github.com>
2023-12-29 00:41:47 +0530

    updated some ui

--- FrontEnd/cart/lib/core/api.dart
+++ FrontEnd/cart/lib/core/api.dart
@@ -1,7 +1,7 @@
 import 'package:dio/dio.dart';
 import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 
-const String BASE_URL = "http://10.169.7.92:5000/api";
+const String BASE_URL = "http://192.168.111.122:5000/api";
 const Map<String, dynamic> DEFAULT_HEADERS = {
   'Content-Type': 'application/json'
 };
--- FrontEnd/cart/lib/presentation/screens/order/order_placed_screen.dart
+++ FrontEnd/cart/lib/presentation/screens/order/order_placed_screen.dart
@@ -1,8 +1,9 @@
+import 'package:cart/presentation/screens/home/home_screen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class OrderPlacedScreen extends StatefulWidget {
-  const OrderPlacedScreen({super.key});
+  const OrderPlacedScreen({Key? key}) : super(key: key);
 
   static const routeName = "order_placed";
 
@@ -13,11 +14,53 @@ class OrderPlacedScreen extends StatefulWidget {
 class _OrderPlacedScreenState extends State<OrderPlacedScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: [Icon(CupertinoIcons.cube_box_fill), Text("Order Placed")],
+          children: [
+            const Icon(
+              CupertinoIcons.cube_box_fill,
+              size: 80,
+              color: Colors.indigo, // You can change the color
+            ),
+            const SizedBox(height: 16),
+            const Text(
+              "Your order has been placed successfully!",
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 24),
+            GestureDetector(
+              onTap: () {
+                Navigator.pushAndRemoveUntil(
+                  context,
+                  MaterialPageRoute(builder: (context) => const HomeScreen()),
+                  (route) => false,
+                );
+              },
+              child: Container(
+                width: 200,
+                padding: const EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  color: Colors.indigo[100],
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: const Center(
+                  child: Text(
+                    "Continue Shopping",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+              ),
+            )
+          ],
         ),
       ),
     );
